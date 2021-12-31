d3f6d8952964b649b0e7349600c0e03d8934378a
zorua162 <jakeirwin03@gmail.com>
2021-12-31 00:21:06 +0000

    Quick config test

--- src/main/java/me/zorua162/heightborder/HeightBorder.java
+++ src/main/java/me/zorua162/heightborder/HeightBorder.java
@@ -2,6 +2,7 @@ package me.zorua162.heightborder;
 
 import me.zorua162.heightborder.border.BorderManager;
 import me.zorua162.heightborder.commands.CommandManager;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HeightBorder extends JavaPlugin {
@@ -21,10 +22,6 @@ public final class HeightBorder extends JavaPlugin {
     // Implement with border "type" being either "break" or "damage"
     //
     // TODO list:
-    // start: start the border moving
-    // stop: stop the border from moving
-    // reorder todo list
-    // edit: edit border's variables
     // save to file
     // load from file on enable
     // display: needs improved configuration
@@ -35,17 +32,28 @@ public final class HeightBorder extends JavaPlugin {
     // Make damage tick config editable
     //
     // Custom death message by checking PlayerDeathEvent
-
+    FileConfiguration config = getConfig();
     public BorderManager borderManager;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        setupConfig();
         borderManager = new BorderManager(this);
         borderManager.setup();
         getCommand("heightborder").setExecutor(new CommandManager(this));
         // TODO reload from file and with saved wbders
     }
+
+    private void setupConfig() {
+        config.addDefault("youAreAwesome", true);
+        config.options().copyDefaults(true);
+        saveConfig();
+        this.getLogger().info("" + config.getBoolean("youAreAwesome"));
+        config.set("youAreAwesome", false);
+        saveConfig();
+    }
+
 
     @Override
     public void onDisable() {
